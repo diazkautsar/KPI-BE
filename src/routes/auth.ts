c5801440e3b1a717654954e610f99032fdff5ef4
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-21 14:01:37 +0700

    initial connect to controllers and use case in routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+// controller
+import AuthController from '../controllers/authController';
+
+// use cases
+import RegisterUseCase from '../useCases/authUseCase/RegisterUseCase';
+
+// libraries
+import HttpResponse from '../libraries/HttpResponse';
+
+const httpResponse = new HttpResponse();
+
+const useCases = {
+    registerUseCase: new RegisterUseCase({}),
+};
+
+const controler = new AuthController({
+    httpResponse,
+    registerUseCase: useCases.registerUseCase,
+});
+
 const router = express.Router();
 
-router.post('/signup', (req: Request, res: Response, next: NextFunction) => {
-    res.send('HELLO FOR SIGNUPO');
+router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await controler.register(req, res, next);
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default router;
