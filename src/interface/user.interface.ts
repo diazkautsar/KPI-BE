aa0408e9c16db3ba82292136ee743fcb96d5adc6
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-21 21:54:28 +0700

    user interface

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,13 @@
+import { Types } from 'mongoose';
+
+export interface UserInterface {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+    user_role: Types.ObjectId;
+    is_active?: boolean;
+    is_blocked?: boolean;
+    created_by?: string;
+    created_id?: string;
+}
