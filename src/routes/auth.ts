388786a75dd6a203a1b806937468ee575b6487d3
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-21 21:53:09 +0700

    add some depencies for register use case

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,18 +1,31 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+// connection
+import { conn } from '../config/database';
+
 // controller
 import AuthController from '../controllers/authController';
 
 // use cases
 import RegisterUseCase from '../useCases/authUseCase/RegisterUseCase';
 
+// repositories
+import UserRoleRepository from '../repositories/UserRole.repository';
+import UserRepository from '../repositories/User.repository';
+
 // libraries
 import HttpResponse from '../libraries/HttpResponse';
+import Bcrypt from '../libraries/Bcrypt';
 
 const httpResponse = new HttpResponse();
 
 const useCases = {
-    registerUseCase: new RegisterUseCase({}),
+    registerUseCase: new RegisterUseCase({
+        userRoleRepository: new UserRoleRepository(),
+        bcrypt: new Bcrypt(),
+        userRepository: new UserRepository(),
+        conn,
+    }),
 };
 
 const controler = new AuthController({
