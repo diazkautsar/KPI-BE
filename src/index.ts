271f92df2d97fd2b4cbb80c49b75eb32b6ba785c
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-21 13:06:52 +0700

    add logger and validate env

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
+import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app';
+import logger from './libraries/logger';
+import { env } from './config/env';
 
 (async () => {
     try {
-        const PORT = 3000
-        const instance = await app()
+        const PORT = parseInt(env.PORT || '3000');
+        const instance = await app();
 
         await instance.listen(PORT, () => {
-            console.log("server running on port ", PORT)
-        })
+            console.log('server running on port ', PORT);
+        });
     } catch (error) {
-        process.exit(1)
+        logger.error(error);
+        process.exit(1);
     }
-})()
+})();
