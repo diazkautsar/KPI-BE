780bd91561c57b36f02a1b12c505dda7291379cf
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-21 21:52:26 +0700

    add conn for transaction

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,3 +6,5 @@ const url = env.DATABASE_URL;
 export const connectDatabase = async () => await mongoose.connect(url);
 
 export const disconnectDatabase = async () => await mongoose.disconnect();
+
+export const conn = mongoose.connection;
