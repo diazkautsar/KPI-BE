c68b6f449e30ff33340d51c848d6270e9ea46aa9
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-22 13:24:08 +0700

    update user model. delete unique constraint

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,14 +3,14 @@ import { UserInterface } from '../interface/user.interface';
 
 export const userSchema = new Schema<UserInterface>({
     name: { type: Schema.Types.String, required: true },
-    email: { type: Schema.Types.String, required: true, unique: true },
-    username: { type: Schema.Types.String, required: true, unique: true },
+    email: { type: Schema.Types.String, required: true },
+    username: { type: Schema.Types.String, required: true },
     password: { type: Schema.Types.String, required: true },
     user_role: { type: Schema.Types.ObjectId, required: true, ref: 'user_role' },
     is_active: { type: Schema.Types.Boolean, required: true, default: true },
     is_blocked: { type: Schema.Types.Boolean, required: true, default: false },
     created_by: { type: Schema.Types.String, required: false },
-    created_id: { type: Schema.Types.ObjectId, required: false },
+    created_by_id: { type: Schema.Types.ObjectId, required: false, ref: 'user' },
 });
 
 export default model<UserInterface>('user', userSchema, 'user');
