faea66853a77c927100558dd9a6ecf89ca0b3f3f
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-22 09:16:28 +0700

    add method for get user based on username or email

--- src/repositories/User.repository.ts
+++ src/repositories/User.repository.ts
@@ -1,6 +1,7 @@
 import { ClientSession } from 'mongoose';
 import userModel from '../models/user.model';
 import { UserInterface } from '../interface/user.interface';
+import { UserRoleInterface } from '../interface/userRole.interface';
 
 export default class UserRepository {
     async addUser(payload: UserInterface, session: ClientSession) {
@@ -8,6 +9,20 @@ export default class UserRepository {
             return await session.withTransaction(async () => await userModel.create(payload));
         } catch (error) {
             throw new Error('Error insert user');
+        }
+    }
+
+    async getUserBasedOnUsernameOrEmail(usernameEmail: string) {
+        try {
+            return await userModel
+                .findOne({
+                    $or: [{ username: usernameEmail }, { email: usernameEmail }],
+                })
+                .populate<{ user_role: UserRoleInterface }>('user_role')
+                .orFail()
+                .exec();
+        } catch (error) {
+            return null;
         }
     }
 }
