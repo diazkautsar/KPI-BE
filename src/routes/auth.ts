88d33e87698962fe009acef249f7b44664f666b3
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-22 09:15:22 +0700

    routes for login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,6 +8,7 @@ import AuthController from '../controllers/authController';
 
 // use cases
 import RegisterUseCase from '../useCases/authUseCase/RegisterUseCase';
+import LoginUseCase from '../useCases/authUseCase/LoginUseCase';
 
 // repositories
 import UserRoleRepository from '../repositories/UserRole.repository';
@@ -16,21 +17,32 @@ import UserRepository from '../repositories/User.repository';
 // libraries
 import HttpResponse from '../libraries/HttpResponse';
 import Bcrypt from '../libraries/Bcrypt';
+import Jwt from '../libraries/Jwt';
 
 const httpResponse = new HttpResponse();
+const userRoleRepository = new UserRoleRepository();
+const userRepository = new UserRepository();
+const bcrypt = new Bcrypt();
+const jwt = new Jwt();
 
 const useCases = {
     registerUseCase: new RegisterUseCase({
-        userRoleRepository: new UserRoleRepository(),
-        bcrypt: new Bcrypt(),
-        userRepository: new UserRepository(),
+        userRoleRepository,
+        bcrypt,
+        userRepository,
         conn,
+    }),
+    loginUseCase: new LoginUseCase({
+        userRepository,
+        bcrypt,
+        jwt,
     }),
 };
 
 const controler = new AuthController({
     httpResponse,
     registerUseCase: useCases.registerUseCase,
+    loginUseCase: useCases.loginUseCase,
 });
 
 const router = express.Router();
@@ -38,6 +50,14 @@ const router = express.Router();
 router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
     try {
         await controler.register(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/signin', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await controler.login(req, res, next);
     } catch (error) {
         next(error);
     }
