349b7e83282e84d6e23dbcf6cd8c461f1de2cf43
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-23 20:52:28 +0700

    acitivty repositories

--- src/repositories/Activity.repository.ts
+++ src/repositories/Activity.repository.ts
@@ -21,4 +21,15 @@ export default class ActivityRepository {
             return [];
         }
     }
+
+    async updateActivityRepository(filter: { [K: string]: any }, update: { [K: string]: any }, session: ClientSession) {
+        try {
+            return await session.withTransaction(async () =>
+                activityModel.findOneAndUpdate(filter, update, { new: true })
+            );
+        } catch (error) {
+            logger.error(`Error update activity ` + error);
+            throw new Error(`Error update activity ` + error);
+        }
+    }
 }
