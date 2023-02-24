fc0e86bfdfeb69933770c1772a6d27e3f616339b
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-24 09:33:11 +0700

    add activities to module

--- src/interface/module.interface.ts
+++ src/interface/module.interface.ts
@@ -3,9 +3,7 @@ import { Types } from 'mongoose';
 export interface ModuleInterface {
     name: string;
     description: string;
-    course: Types.ObjectId;
-    cover_image?: string;
-    keyword?: string;
+    activities: Types.ObjectId[];
     is_active?: boolean;
     created_by?: string;
     created_by_id?: string;
