5ca6a5398b6faffe685fa5180403009870a08c85
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-24 14:28:58 +0700

    create course route and controller

---
+++ src/controllers/courseController/index.ts
@@ -0,0 +1,98 @@
+import { Response, NextFunction } from 'express';
+
+import HttpResponse from '../../libraries/HttpResponse';
+import { CustomRequest } from '../../interface/request.interface';
+import Utililty from '../../utilities';
+
+import CreateCourseUseCase from '../../useCases/courseUseCase/CreateCourseUseCase';
+import GetCourseUseCase from '../../useCases/courseUseCase/GetCourseUseCase';
+import UpdateCourseUseCase from '../../useCases/courseUseCase/UpdateCourseUseCase';
+
+type constructorType = {
+    httpResponse: HttpResponse;
+    createCourseUseCase: CreateCourseUseCase;
+    getCourseUseCase: GetCourseUseCase;
+    updateCourseUseCase: UpdateCourseUseCase;
+};
+
+export default class CourseController {
+    httpResponse: HttpResponse;
+    createCourseUseCase: CreateCourseUseCase;
+    getCourseUseCase: GetCourseUseCase;
+    updateCourseUseCase: UpdateCourseUseCase;
+
+    constructor(args: constructorType) {
+        this.httpResponse = args.httpResponse;
+        this.createCourseUseCase = args.createCourseUseCase;
+        this.getCourseUseCase = args.getCourseUseCase;
+        this.updateCourseUseCase = args.updateCourseUseCase;
+    }
+
+    async createCourse(req: CustomRequest, res: Response, next: NextFunction) {
+        const payload = {
+            body: req.body,
+            user: req.user,
+        };
+        const response = await this.createCourseUseCase.exec(payload);
+
+        this.httpResponse.httpResponse(response, res);
+    }
+
+    async getCourseById(req: CustomRequest, res: Response, next: NextFunction) {
+        let is_active: boolean = true;
+        const id = req.params.courseId;
+        if (!id) {
+            next({
+                statusCode: 400,
+                message: 'Invalid activity id',
+                messageTitle: 'Bad request',
+            });
+        }
+
+        if (req.query.is_active) {
+            const data = req.query.is_active as string;
+            is_active = Utililty.trueOrFalse(data);
+        }
+
+        const payload = [id];
+
+        // const response = await this.getCourseUseCase.exec(payload, is_active);
+
+        // this.httpResponse.httpResponse(response, res);
+    }
+
+    async getCourses(req: CustomRequest, res: Response, next: NextFunction) {
+        let is_active: boolean = true;
+        if (req.query.is_active) {
+            const data = req.query.is_active as string;
+            is_active = Utililty.trueOrFalse(data);
+        }
+
+        // const response = await this.getCourseUseCase.exec(null, is_active);
+
+        // this.httpResponse.httpResponse(response, res);
+    }
+
+    async updateCourse(req: CustomRequest, res: Response, next: NextFunction) {
+        const payload = {
+            body: req.body,
+        };
+
+        // const response = await this.updateModuleUseCase.exec(payload);
+
+        // this.httpResponse.httpResponse(response, res);
+    }
+
+    async deleteCourse(req: CustomRequest, res: Response, next: NextFunction) {
+        const payload = {
+            body: {
+                is_active: false,
+                id: req.params.activityId,
+            },
+        };
+
+        // const response = await this.updateCourseUseCase.exec(payload);
+
+        // this.httpResponse.httpResponse(response, res);
+    }
+}
---
+++ src/routes/course.ts
@@ -0,0 +1,101 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+// connection
+import { conn } from '../config/database';
+
+// controller
+import CourseController from '../controllers/courseController';
+
+// use cases
+import CreateCourseUseCase from '../useCases/courseUseCase/CreateCourseUseCase';
+import GetCourseUseCase from '../useCases/courseUseCase/GetCourseUseCase';
+import UpdateCourseUseCase from '../useCases/courseUseCase/UpdateCourseUseCase';
+
+// repositories
+import CourseRepository from '../repositories/Course.repository';
+import ModuleRepository from '../repositories/Module.repository';
+
+// libraries
+import HttpResponse from '../libraries/HttpResponse';
+
+// middlewares
+import { authMiddleware, permissionRole } from '../middlewares/index';
+
+// constant
+import { ROLE_ADMINISTRATOR, ROLE_LEARNER, ROLE_PROVIDER } from '../constants/index';
+import { CustomRequest } from '../interface/request.interface';
+
+const httpResponse = new HttpResponse();
+const courseRepository = new CourseRepository();
+const moduleRepository = new ModuleRepository();
+
+const useCases = {
+    createCourseUseCase: new CreateCourseUseCase({
+        conn,
+        courseRepository,
+        moduleRepository,
+    }),
+    getCourseUseCase: new GetCourseUseCase(),
+    updateCourseUseCase: new UpdateCourseUseCase(),
+};
+
+const controller = new CourseController({
+    httpResponse,
+    createCourseUseCase: useCases.createCourseUseCase,
+    getCourseUseCase: useCases.getCourseUseCase,
+    updateCourseUseCase: useCases.updateCourseUseCase,
+});
+
+const router = express.Router();
+
+router.post(
+    '/',
+    authMiddleware,
+    permissionRole([ROLE_ADMINISTRATOR, ROLE_PROVIDER]),
+    async (req: CustomRequest, res: Response, next: NextFunction) => {
+        try {
+            await controller.createCourse(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get('/:courseId', authMiddleware, async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        await controller.getCourseById(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/', authMiddleware, async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        await controller.getCourses(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/', authMiddleware, async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        await controller.updateCourse(req, res, next);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete(
+    '/:courseId',
+    authMiddleware,
+    permissionRole([ROLE_ADMINISTRATOR, ROLE_PROVIDER]),
+    async (req: CustomRequest, res: Response, next: NextFunction) => {
+        try {
+            await controller.deleteCourse(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+export default router;
